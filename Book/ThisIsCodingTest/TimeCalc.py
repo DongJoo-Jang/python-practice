b2d1859dfe53473da2d2bfc5f7aa27e298912f6e
Sup <svra0945@gmail.com>
2024-12-14 18:32:14 +0900

    python-practice start

--- Book/ThisIsCodingTest/TimeCalc.py
+++ Book/ThisIsCodingTest/TimeCalc.py
@@ -1,3 +1,7 @@
+import time
+start_time = time.time()
+
+# 프로그램 소스코드
 array = [3,5,1,2,4] #
 
 summary = 0
@@ -5,11 +9,10 @@ summary = 0
 # 모든 데이터 더하면서 합계
 for x in array :
     summary += x
-#결과물 출력
-print(summary)
 
 for i in array :
     for j in array :
         temp = i * j
 
-        print(temp)
+end_time = time.time() # 측정 종료
+print("time : ", end_time - start_time)
